c691634b6b67d9eb782a0d30dcbf8f61c2cfcab3
k.manas.k <manassyildirim@gmail.com>
2025-01-08 20:41:39 +0100

    Update mysql-connection.js

--- mysql-connection.js
+++ mysql-connection.js
@@ -1,10 +1,10 @@
 import mysql from 'mysql2';
 
 const connection = mysql.createConnection({
-  host: '',       // Replace with your MySQL host (localhost if running locally)
-  user: '',            // Replace with your MySQL username (root is default)
-  password: '',  // Replace with your MySQL password
-  database: '' // Replace with your MySQL database name
+  host: process.env.MYSQL_HOST,       // Replace with your MySQL host (localhost if running locally)
+  user: process.env.MYSQL_USER,            // Replace with your MySQL username (root is default)
+  password: process.env.MYSQL_PASS,   // Replace with your MySQL password
+  database: process.env.MYSQL_DATABASE // Replace with your MySQL database name
 });
 
 // Test the connection
