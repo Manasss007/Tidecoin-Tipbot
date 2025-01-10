b134e29321bf7a200beba34e8b571c9ae51740ad
k.manas.k <manassyildirim@gmail.com>
2025-01-10 14:08:55 +0100

    Update mysql-connection.js

--- mysql-connection.js
+++ mysql-connection.js
@@ -1,9 +1,12 @@
 import mysql from 'mysql2';
+import dotenv from 'dotenv';
 
-const connection = mysql.createConnection({
+dotenv.config(); // Load environment variables
+
+export const connection = mysql.createConnection({
   host: process.env.MYSQL_HOST,       // Replace with your MySQL host (localhost if running locally)
   user: process.env.MYSQL_USER,            // Replace with your MySQL username (root is default)
-  password: process.env.MYSQL_PASS,   // Replace with your MySQL password
+  password: process.env.MYSQL_PASS ,  // Replace with your MySQL password
   database: process.env.MYSQL_DATABASE // Replace with your MySQL database name
 });
 
